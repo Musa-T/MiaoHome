3b39fb8a8e405e858c5a89522a34bc93eb4c8880
Pit-T <125972483+Pit-T@users.noreply.github.com>
2024-01-15 14:06:11 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,16 +2,16 @@ export default {
 
     // 👇页面内容相关配置
     // 名字
-    Name: "MiaoMint",
+    Name: "Musa",
 
     // 头像
     Avatar: "https://q1.qlogo.cn/g?b=qq&k=G1ibIlnicR8Ij4CZO3BF6cVA&s=640",
 
     // 介绍
-    Desc: "👋 Hi! 这里是 MiaoMint 的主页",
+    Desc: "👋 Hi! 这里是 Musa 的主页",
 
     // Github 用户名
-    GithubUsername: "MiaoMint",
+    GithubUsername: "Musa",
 
     // 博客RSS订阅
     BlogRSS: "https://blog.0u0.ren/feed/",
@@ -26,7 +26,7 @@ export default {
     TwitterUrl: "https://twitter.com/MiaoMint_",
 
     // B站UID
-    BilibiliUid: "52625050",
+    BilibiliUid: "384298756",
 
     // 默认 /home.md 
     // null 则不显示
@@ -36,16 +36,16 @@ export default {
     // 网页信息
     SiteHead: {
         // 站点标题
-        Title: "MiaoMint 的主页",
+        Title: "Musa 的主页",
 
         // 站点图标
         Favicon: "https://q1.qlogo.cn/g?b=qq&k=G1ibIlnicR8Ij4CZO3BF6cVA&s=640",
 
         // 站点关键词 多个使用 “,” 分割
-        KeyWords: "MiaoMint,Erhecy,喵薄荷,主页,g8ink,短链接,博客",
+        KeyWords: "Musa,Erhecy,喵薄荷,主页,g8ink,短链接,博客",
 
         // 站点介绍
-        Desc: "👋 Hi! 这里是 MiaoMint 的主页",
+        Desc: "👋 Hi! 这里是 Musa 的主页",
     },
 
     SiteBackground: "url(https://img.truimo.com/2022/807b270630a51.jpg) center/cover",
